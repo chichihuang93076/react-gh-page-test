8382348f07b65a406017d0273d60e4503fba23f0
chichi <chichihuang93076@gmail.com>
2023-08-25 11:27:23 +0800

    route

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,19 @@ import Day5 from "./components/Day5";
 import Week1 from "./components/Week1";
 import Week2 from "./components/Week2";
 import Week3 from "./components/Week3";
+import SignIn from "./components/SignIn";
 
 function App() {
   return (
     <div className="App">
       <NavComponent />
       <Routes>
-        <Route path="/" element={<Week1 />} />
+        <Route path="/" element={<Week3 />} />
         <Route path="/daywork" element={<Day5 />} />
         <Route path="/week1" element={<Week1 />} />
         <Route path="/week2" element={<Week2 />} />
         <Route path="/Week3" element={<Week3 />} />
+        <Route path="/signin" element={<SignIn />} />
       </Routes>
     </div>
   );
